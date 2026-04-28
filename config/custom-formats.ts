203eae4c08298f8209ac2ff997b4207f293a8c43
Clark Jones <clarkj338@gmail.com>
2026-04-28 12:52:20 +0530

    Create custom-formats.ts

---
+++ config/custom-formats.ts
@@ -0,0 +1,18 @@
+export const Formats: FormatList = [
+	{
+		section: "S/V Singles",
+	},
+	{
+		name: "[Gen 9] PokeRogue Campaign",
+		mod: 'pokerogue',
+		ruleset: [
+			'Obtainable',
+			'Cancel Mod',
+			'HP Percentage Mod',
+			'pokeroguerules',
+		],
+		defaultLevel: 5,
+		maxLevel: 200,
+		banlist: [],
+	},
+];
